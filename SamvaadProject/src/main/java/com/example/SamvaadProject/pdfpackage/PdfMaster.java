204485283a3aebd5386bbf2e58b90aa9d621875d
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-09-24 22:04:10 +0530

    Update PdfMaster.java

--- SamvaadProject/src/main/java/com/example/SamvaadProject/pdfpackage/PdfMaster.java
+++ SamvaadProject/src/main/java/com/example/SamvaadProject/pdfpackage/PdfMaster.java
@@ -1,8 +1,9 @@
 package com.example.SamvaadProject.pdfpackage;
 
-import com.example.SamvaadProject.batchmasterpackage.BatchMaster;
 import com.example.SamvaadProject.coursepackage.CourseMaster;
 import jakarta.persistence.*;
+
+import java.util.Date;
 
 @Entity
 @Table(name = "pdf_master")
@@ -15,11 +16,33 @@ public class PdfMaster {
     @JoinColumn(name = "courseId")
     private CourseMaster course;
 
+    @Column(name="documentname")
+    private String documentName;
+
+    private Date uploadedAt;
+
+
 //    @ManyToOne
 //    @JoinColumn(name = "batchId")
 //    private PdfMaster pdfs;
 
     private byte [] documentPath;
+
+    public String getDocumentName() {
+        return documentName;
+    }
+
+    public Date getUploadedAt() {
+        return uploadedAt;
+    }
+
+    public void setUploadedAt(Date uploadedAt) {
+        this.uploadedAt = uploadedAt;
+    }
+
+    public void setDocumentName(String documentName) {
+        this.documentName = documentName;
+    }
 
     public Long getPdfId() {
         return pdfId;

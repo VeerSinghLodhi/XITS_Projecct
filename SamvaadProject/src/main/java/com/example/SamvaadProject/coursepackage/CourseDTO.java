134979413fc67c7375c3cd297b71dad2115fce24
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-09-24 22:13:31 +0530

    Add files via upload

---
+++ SamvaadProject/src/main/java/com/example/SamvaadProject/coursepackage/CourseDTO.java
@@ -0,0 +1,23 @@
+package com.example.SamvaadProject.coursepackage;
+
+public class CourseDTO {
+
+    private Double courseFee;
+    private Double balance;
+
+    public Double getCourseFee() {
+        return courseFee;
+    }
+
+    public void setCourseFee(Double courseFee) {
+        this.courseFee = courseFee;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+}

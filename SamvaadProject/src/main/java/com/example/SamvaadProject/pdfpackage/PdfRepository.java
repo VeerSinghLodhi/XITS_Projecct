e312fca4d062b0250c12594224977cf089ac05ea
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-09-24 22:04:25 +0530

    Update PdfRepository.java

--- SamvaadProject/src/main/java/com/example/SamvaadProject/pdfpackage/PdfRepository.java
+++ SamvaadProject/src/main/java/com/example/SamvaadProject/pdfpackage/PdfRepository.java
@@ -1,8 +1,13 @@
 package com.example.SamvaadProject.pdfpackage;
 
+import com.example.SamvaadProject.coursepackage.CourseMaster;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+
+
 public interface PdfRepository extends JpaRepository<PdfMaster, Long> {
+    List<PdfMaster> findByCourse_CourseId(Long courseId);
+
 }

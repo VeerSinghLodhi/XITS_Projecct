5499998c3d8b00ba240d83e32f9aed43eab5e2a4
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-10-02 14:06:48 +0530

    Update UserController.java

--- SamvaadProject/src/main/java/com/example/SamvaadProject/usermasterpackage/UserController.java
+++ SamvaadProject/src/main/java/com/example/SamvaadProject/usermasterpackage/UserController.java
@@ -3,15 +3,24 @@ package com.example.SamvaadProject.usermasterpackage;
 import com.example.SamvaadProject.admissionpackage.AdmissionDTO;
 import com.example.SamvaadProject.admissionpackage.AdmissionMaster;
 import com.example.SamvaadProject.admissionpackage.AdmissionRepository;
+import com.example.SamvaadProject.assignmentpackage.AssignmentMaster;
+import com.example.SamvaadProject.assignmentpackage.AssignmentRepository;
+import com.example.SamvaadProject.assignmentpackage.SubmitAssignment;
+import com.example.SamvaadProject.assignmentpackage.SubmitRepository;
 import com.example.SamvaadProject.batchmasterpackage.BatchMaster;
 import com.example.SamvaadProject.batchmasterpackage.BatchMasterRepository;
 import com.example.SamvaadProject.coursepackage.CourseMaster;
 import com.example.SamvaadProject.coursepackage.CourseRepository;
 import com.example.SamvaadProject.emailservicespackage.EmailService;
 import com.example.SamvaadProject.feespackage.FeePayment;
+import com.example.SamvaadProject.pdfpackage.PdfDTO;
+import com.example.SamvaadProject.pdfpackage.PdfMaster;
+import com.example.SamvaadProject.pdfpackage.PdfRepository;
+import com.example.SamvaadProject.studentbatchpackage.StudentBatchRepository;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -20,9 +29,13 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @Controller
@@ -46,6 +59,18 @@ public class UserController {
     @Autowired
     AdmissionRepository admissionRepository;
 
+    @Autowired
+    StudentBatchRepository studentBatchRepository;
+
+    @Autowired
+    SubmitRepository submitRepository;
+
+    @Autowired
+    AssignmentRepository assignmentRepository;
+
+    @Autowired
+    PdfRepository pdfRepository;
+
     @GetMapping("/login")
     public String showLoginPage() {
         return "login";
@@ -60,6 +85,11 @@ public class UserController {
 
         if (user == null) {
             model.addAttribute("error", "Invalid username or password!");
+            return "login";
+        }
+
+        if(!user.getStatus()){
+            model.addAttribute("error","This account has been disabled by the admin!!");
             return "login";
         }
 
@@ -93,7 +123,8 @@ public class UserController {
                           @RequestParam(value = "studentsMapped",required = false)Boolean isStudentsMapped,
                           @RequestParam(value = "feeSubmitted",required = false)Boolean isFeeSubmitted,
                           @RequestParam(value = "studyMaterialUploaded",required = false)Boolean isStudyMaterialUploaded,
-                          @RequestParam(value = "notePdfDeleted",required = false)Boolean isNotePdfDeleted){
+                          @RequestParam(value = "notePdfDeleted",required = false)Boolean isNotePdfDeleted,
+                          @RequestParam(value = "facultyUpdated",required = false)Boolean isFacultyUpdated){
 
         Long userId=(Long) session.getAttribute("userId");
         UserMaster userMaster=userRepository.findById(userId).orElse(null);
@@ -131,6 +162,9 @@ public class UserController {
         }
         if (Boolean.TRUE.equals(isNotePdfDeleted)) {
             model.addAttribute("notePdfDeleted", true);
+        }
+        if (Boolean.TRUE.equals(isFacultyUpdated)) {
+            model.addAttribute("facultyUpdated", true);
         }
 
 
@@ -201,6 +235,50 @@ public class UserController {
             model.addAttribute("error","Session expired!");
             return "login";
         }
+        List<Long> batchIds1 = admissionRepository.findByUserMaster_UserId(userId)
+                .stream()
+                .map(ad -> studentBatchRepository.findByAdmission_AdmissionId(ad.getAdmissionId())
+                        .getBatch()
+                        .getBatchId())
+                .toList();
+
+        List<BatchMaster> batches1 = batchMasterRepository.findAllById(batchIds1);
+        model.addAttribute("batches", batches1);
+
+////
+//        // Student batches
+//        List<BatchMaster> batches = batchMasterRepository.findBatchesByStudent(userId);
+//        List<Long> batchIds = batches.stream().map(BatchMaster::getBatchId).collect(Collectors.toList());
+//
+//
+//        // Assignments in batches
+//        List<AssignmentMaster> assignments = batchIds.isEmpty()
+//                ? Collections.emptyList()
+//                : assignmentRepository.findByBatch_BatchIdIn(batchIds);
+//
+//        // Submitted assignments
+//        List<SubmitAssignment> submittedAssignments = submitRepository
+//                .findByAdmission_UserMaster_UserId(userId);
+//
+//        List<Long> submittedAssignmentIds = submittedAssignments.stream()
+//                .map(sa -> sa.getAssignment().getAssignmentId())
+//                .collect(Collectors.toList());
+//
+//        // Map to check delete eligibility (within 24 hours)
+//        Map<Long, Boolean> deletableMap = submittedAssignments.stream()
+//                .collect(Collectors.toMap(
+//                        sa -> sa.getAssignment().getAssignmentId(),
+//                        sa -> sa.getSubmittedAt() != null &&
+//                                (System.currentTimeMillis() - sa.getSubmittedAt().getTime()) < 86400000
+//                ));
+//
+//        model.addAttribute("assignments", assignments);
+//        model.addAttribute("submittedAssignmentIds", submittedAssignmentIds);
+//        model.addAttribute("deletableMap", deletableMap);
+
+//
+
+
         model.addAttribute("user_master",userMaster);
         return "StudentHTMLs/studentdashboard";
     }
@@ -230,6 +308,7 @@ public class UserController {
             System.out.println("Username is "+getGenerateUsername(newUser.getFullName(),newUser.getDob()));
             newUser.setUsername(getGenerateUsername(newUser.getFullName(),newUser.getDob()));
             newUser.setPassword("Xits@143");
+            newUser.setStatus(true);
 
             Long adminId=(Long) session.getAttribute("userId");
             UserMaster admin=userRepository.findById(adminId).orElse(null);
@@ -237,8 +316,8 @@ public class UserController {
                 model.addAttribute("error","Session expired!");
                 return "login";
             }
-            redirectAttributes.addAttribute("newUserAdded",true);
             userRepository.save(newUser);
+            redirectAttributes.addAttribute("newUserAdded",true);
             emailService.getSendUsernameAndPassword(newUser.getFullName(),newUser.getUsername(),newUser.getPassword(),newUser.getEmail());
             return "redirect:/admin/dashboard";
 
@@ -255,10 +334,9 @@ public class UserController {
         return (uName.length() >= 4 ? uName.substring(0, 4) : uName)+""+(userRepository.getMaxCount()+1)+""+dob.getYear();
     }
 
-    @GetMapping("/userphoto")
-    ResponseEntity<byte[]>getUserProfile(HttpSession session,Model model){
+    @GetMapping("/userphoto/{userId}")
+    ResponseEntity<byte[]>getUserProfile(@PathVariable("userId")Long userId){
 
-        Long userId=(Long) session.getAttribute("userId");
         UserMaster userMaster=userRepository.findById(userId).orElse(null);// Admin Object for session creation
         if(userMaster==null){
             return ResponseEntity.notFound().build();
@@ -268,6 +346,100 @@ public class UserController {
                 .body(userMaster.getPhotoPath());
 
     }
+
+
+    // For get Faculty as well as student Data
+    @GetMapping("/faculty/data/{facultyId}")
+    @ResponseBody
+    public FacultyDTO getFacultyData(@PathVariable("facultyId")Long facultyId){
+         UserMaster faculty= userRepository.findById(facultyId).orElse(null);
+
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+         FacultyDTO facultyDTO=new FacultyDTO();
+         facultyDTO.setFacultyId(faculty.getUserId());
+         facultyDTO.setFacultyName(faculty.getFullName());
+         facultyDTO.setFacultyEmail(faculty.getEmail());
+         facultyDTO.setFacultyContactNo(faculty.getContactNo());
+         facultyDTO.setFacultyDOB(faculty.getDob());
+         facultyDTO.setFacultyAddress(faculty.getAddress());
+         facultyDTO.setFacultyCity(faculty.getCity());
+         facultyDTO.setFacultyStatus(faculty.getStatus().toString());
+         facultyDTO.setPhoto(faculty.getPhotoPath());
+         System.out.println("Faculty Status "+faculty.getStatus().toString());
+
+
+         return facultyDTO;
+    }
+
+    // For Faculty as well as student update
+    @PostMapping("/faculty/update")
+    public String getUpdate(@RequestParam("facultySelectId")Long facultyId,
+                            @ModelAttribute("newUser")UserMaster facultyData,
+                            @RequestParam("facultyPhoto")MultipartFile facultyPhoto,
+                            Model model,
+                            RedirectAttributes redirectAttributes){
+            try {
+                UserMaster updateFaculty = userRepository.findById(facultyId).orElse(null);
+                updateFaculty.setFullName(facultyData.getFullName());
+                updateFaculty.setEmail(facultyData.getEmail());
+                updateFaculty.setContactNo(facultyData.getContactNo());
+                updateFaculty.setDob(facultyData.getDob());
+                updateFaculty.setAddress(facultyData.getAddress());
+                updateFaculty.setCity(facultyData.getCity());
+                updateFaculty.setStatus(facultyData.getStatus());  // IMP!!
+
+                if (!facultyPhoto.isEmpty()) {
+                    updateFaculty.setPhotoPath(facultyPhoto.getBytes());
+                }
+
+                userRepository.save(updateFaculty);
+
+
+                redirectAttributes.addAttribute("facultyUpdated",true);
+
+                return "redirect:/admin/dashboard";
+
+            }catch(Exception e){
+                System.out.println("Error is "+e.getMessage());
+                model.addAttribute("error","Something went wrong!");
+                return "login";
+            }
+
+//        System.out.println("Faculty Id "+facultyId);
+//        System.out.println("Faculty Name "+facultyData.getFullName());
+//        System.out.println("Faculty DOB "+facultyData.getDob());
+//        System.out.println("Faculty Address "+facultyData.getAddress());
+//        System.out.println("Faculty Status "+facultyData.getStatus());
+//        System.out.println("Faculty File "+facultyPhoto.getOriginalFilename());
+
+    }
+
+
+    @GetMapping("/student/course/material/batch/{batchId}")
+    @ResponseBody
+    public List<PdfDTO> getMaterialByBatch(@PathVariable("batchId")Long batchId){
+        Long courseId=batchMasterRepository.findById(batchId).orElse(null).getCourse().getCourseId();
+        return pdfRepository.findByCourse_CourseId(courseId)
+                .stream()
+                .map(pdf->new PdfDTO(pdf.getDocumentName(),pdf.getUploadedAt().toString(),pdf.getPdfId()))
+                .toList();
+    }
+
+//    Download Notes
+@GetMapping("/student/notes/download/{pdfId}")
+public ResponseEntity<byte[]> getResume(@PathVariable("pdfId")Long pdfId) {
+
+    PdfMaster pdfMaster=pdfRepository.findById(pdfId).orElse(null);
+    if (pdfMaster == null || pdfMaster.getDocumentPath() == null) {
+        return ResponseEntity.notFound().build();
+    }
+
+    return ResponseEntity.ok()
+            .contentType(MediaType.APPLICATION_PDF) // or whatever format you accept
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+pdfMaster.getDocumentName()+"\"")
+            .body(pdfMaster.getDocumentPath());
+}
 
 
 }

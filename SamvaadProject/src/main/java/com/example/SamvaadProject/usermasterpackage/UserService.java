d9c1cbbadc336ceb848d8bc294c358854777360d
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-10-14 01:34:42 +0530

    Update UserService.java

--- SamvaadProject/src/main/java/com/example/SamvaadProject/usermasterpackage/UserService.java
+++ SamvaadProject/src/main/java/com/example/SamvaadProject/usermasterpackage/UserService.java
@@ -1,6 +1,7 @@
 package com.example.SamvaadProject.usermasterpackage;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -14,5 +15,28 @@ public class UserService {
     public UserMaster login(String username, String password) {
         Optional<UserMaster> user = userRepository.findByUsernameAndPassword(username, password);
         return user.orElse(null);
+    }
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public String updatePassword(String username, String currentPassword, String newPassword) {
+//        System.out.println("Username "+username);
+        UserMaster user = userRepository.findByUsername(username);
+//        System.out.println("Inside the Update Password Faculty Service Method");
+        // Verify current password
+        if (!passwordEncoder.matches(currentPassword, user.getPassword())) {
+            return "currentFalse";
+        }
+
+        if (passwordEncoder.matches(newPassword, user.getPassword())) {
+            return "currentSame";
+        }
+        // Encode and set new password
+        user.setPassword(passwordEncoder.encode(newPassword));
+
+        // Save user
+        userRepository.save(user);
+        return "updated";
     }
 }

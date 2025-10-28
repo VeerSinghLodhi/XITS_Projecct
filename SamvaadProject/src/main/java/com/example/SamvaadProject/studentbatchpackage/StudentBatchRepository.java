6b7f06bc70f35460894d78a63d4b2e13d6713909
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-10-28 10:35:53 +0530

    Update StudentBatchRepository.java

--- SamvaadProject/src/main/java/com/example/SamvaadProject/studentbatchpackage/StudentBatchRepository.java
+++ SamvaadProject/src/main/java/com/example/SamvaadProject/studentbatchpackage/StudentBatchRepository.java
@@ -3,6 +3,8 @@ package com.example.SamvaadProject.studentbatchpackage;
 import com.example.SamvaadProject.admissionpackage.AdmissionMaster;
 import com.example.SamvaadProject.batchmasterpackage.BatchMaster;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +15,15 @@ public interface StudentBatchRepository extends JpaRepository<StudentBatchMap,Lo
     StudentBatchMap findByAdmission_AdmissionId(String admissionID);
     List<StudentBatchMap> findByBatch_Course_CourseId(Long courseId);
 //    List<AdmissionMaster> findByUserMaster_UserId(Long userId);
+
+//    @Query("SELECT sbm.batch FROM StudentBatchMap sbm " +
+//            "JOIN sbm.admission a " +
+//            "WHERE a.userMaster.userId = :userId")
+//        List<BatchMaster> findBatchesByStudentUserId(@Param("userId") Long userId);
+@Query("SELECT sbm.batch FROM StudentBatchMap sbm " +
+        "JOIN sbm.admission a " +
+        "WHERE a.userMaster.userId = :userId " +
+        "ORDER BY CASE WHEN sbm.batch.status = 'ARCHIVED' THEN 1 ELSE 0 END, sbm.batch.batchId DESC")
+List<BatchMaster> findBatchesByStudentUserId(@Param("userId") Long userId);
+
 }

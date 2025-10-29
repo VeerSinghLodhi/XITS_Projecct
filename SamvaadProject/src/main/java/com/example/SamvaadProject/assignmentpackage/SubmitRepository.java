756df04dfd46fc0a32d6c6ff99b403afe11b1c8a
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-10-29 22:29:11 +0530

    Update SubmitRepository.java

--- SamvaadProject/src/main/java/com/example/SamvaadProject/assignmentpackage/SubmitRepository.java
+++ SamvaadProject/src/main/java/com/example/SamvaadProject/assignmentpackage/SubmitRepository.java
@@ -4,6 +4,8 @@ import com.example.SamvaadProject.assignmentpackage.SubmitAssignment;
 import com.example.SamvaadProject.admissionpackage.AdmissionMaster;
 import com.example.SamvaadProject.assignmentpackage.AssignmentMaster;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,4 +20,16 @@ public interface SubmitRepository extends JpaRepository<SubmitAssignment, Long> 
     // Find a submission for a specific assignment and admission
     Optional<SubmitAssignment> findByAssignmentAndAdmission(AssignmentMaster assignment, AdmissionMaster admission);
     List<SubmitAssignment> findByAssignment_AssignmentId(Long assignmentId);
+
+
+    @Query("SELECT sa.assignment.assignmentId FROM SubmitAssignment sa WHERE sa.admission.admissionId = :admissionId")
+    List<Long> findSubmittedAssignmentIdsByAdmissionId(@Param("admissionId") Long admissionId);
+
+    @Query("""
+        SELECT COUNT(s)
+        FROM SubmitAssignment s
+        WHERE s.admission.userMaster.userId = :userId
+    """)
+    Long countSubmissionsByUserId(@Param("userId") Long userId);
+
 }
